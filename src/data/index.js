2d5e3bca0164863304bcaf275a278e5fc6e3597b
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-09-11 16:51:48 +0500

    porogramist

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,351 @@
-export const quiz = [
-    
-]
+export const quizes = [
+  {
+    main_title: "FullStack",
+    questions: [
+      {
+        id: 1,
+        question_title: "Что означает HTML?",
+        answers: [
+          {
+            answer1: "HYPERTEXT MONKEY LANGUAGE",
+            answer_status: false,
+          },
+          {
+            answer2: "HOPE THIS MAKES LASAGNE",
+            answer_status: false,
+          },
+          {
+            answer3: "HOW TO MEET LADIES",
+            answer_status: false,
+          },
+          {
+            answer4: "HYPERTEXT MARKUP LANGUAGE",
+            answer_status: true,
+          },
+        ],
+      },
+      {
+        id: 2,
+        question_title: "Как в python выводят текст на экран?",
+        answers: [
+          {
+            answer1: "PRINT('HELLO WORLD')",
+            answer_status: true,
+          },
+          {
+            answer2: "DOCUMENT.WRITE('HELLO WORLD')",
+            answer_status: false,
+          },
+          {
+            answer3: "CONSOLE.LOG('HELLO WORLD')",
+            answer_status: false,
+          },
+          {
+            answer4: "OUTPUT('HELLO WORLD')",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 3,
+        question_title: "Какой тег в HTML создает заголовок?",
+        answers: [
+          {
+            answer1: "<FOOTER>",
+            answer_status: false,
+          },
+          {
+            answer2: "<H1>",
+            answer_status: true,
+          },
+          {
+            answer3: "<P>",
+            answer_status: false,
+          },
+          {
+            answer4: "<IMG>",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 4,
+        question_title: "Что такое цикл в программировании?",
+        answers: [
+          {
+            answer1: "ТАНЦЕВАЛЬНОЕ ДВИЖЕНИЕ",
+            answer_status: false,
+          },
+          {
+            answer2: "ПОВТОРЯЮЩИЙСЯ ФРАГМЕНТ КОДА",
+            answer_status: true,
+          },
+          {
+            answer3: "ОШИБКА В КОДЕ",
+            answer_status: false,
+          },
+          {
+            answer4: "СПОСОБ ВЫВОДА ДАННЫХ",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 5,
+        question_title: "Где хранятся данные в программировании?",
+        answers: [
+          {
+            answer1: "В ПЕРЕМЕННЫХ",
+            answer_status: false,
+          },
+          {
+            answer2: "В ФУНКЦИЯХ",
+            answer_status: false,
+          },
+          {
+            answer3: "В БАЗЕ ДАННЫХ",
+            answer_status: true,
+          },
+          {
+            answer4: "В КЛАССАХ",
+            answer_status: false,
+          },
+        ],
+      },
+      // Add more questions here
+    ],
+  },
+  {
+    id: 6,
+    main_title: "Data Science",
+    questions: [
+      {
+        question_title: "JavaScript is a statically typed language.",
+        answers: [
+          {
+            answer1: "JavaScript",
+            answer_status: true,
+          },
+          {
+            answer2: "JavaScript",
+            answer_status: false,
+          },
+          {
+            answer3: "JavaScript",
+            answer_status: false,
+          },
+          {
+            answer4: "JavaScript",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 7,
+        question_title:
+          "Как называются основной объект для создания. моделей в 3ds MAX?",
+        answers: [
+          {
+            answer1: "MESH",
+            answer_status: true,
+          },
+          {
+            answer2: "SPLINE",
+            answer_status: false,
+          },
+          {
+            answer3: "CAMERA",
+            answer_status: false,
+          },
+          {
+            answer4: "LIGHT",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 8,
+        question_title: "Что нужно сделать перед рендерингом сцены?",
+        answers: [
+          {
+            answer1: "СОЗДАТЬ МАТЕРИАЛЫ И ТЕКСТУРЫ",
+            answer_status: false,
+          },
+          {
+            answer2: "АСТРОИТЬ ОСВЕЩЕНИЕ",
+            answer_status: false,
+          },
+          {
+            answer3: "УСТОНОВИТЬ КАМЕРУ",
+            answer_status: true,
+          },
+          {
+            answer4: "ВСЕ ОТВЕТЫ ВЕРНЫ",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 9,
+        question_title: "Где настраиваются материалы в 3ds MAX?",
+        answers: [
+          {
+            answer1: "В РЕДАКТОРЕ МАТЕРИАЛОВ",
+            answer_status: true,
+          },
+          {
+            answer2: "В ОКНЕ MODIFY",
+            answer_status: false,
+          },
+          {
+            answer3: "ВО ВКЛАДКЕ RENDERING",
+            answer_status: false,
+          },
+          {
+            answer4: "В МЕНЮ CUSTOMIZE",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 10,
+        question_title: "Что нужно сделать перед рендерингом сцены?",
+        answers: [
+          {
+            answer1: "СОЗДАТЬ МАТЕРИАЛЫ И ТЕКСТУРЫ",
+            answer_status: false,
+          },
+          {
+            answer2: "НАСТРОИТЬ ОСВЕЩЕНИЕ",
+            answer_status: false,
+          },
+          {
+            answer3: "УСТОНОВИТЬ КАМЕРУ",
+            answer_status: true,
+          },
+          {
+            answer4: "ВСЕ ОТВЕТЫ ВЕРНЫ",
+            answer_status: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    main_title: "Software Engineering",
+    questions: [
+      {
+        id: 11,
+        question_title:
+          "Какой протокол используется для передачи веб-страниц и файлов?",
+        answers: [
+          {
+            answer1: "FTP",
+            answer_status: false,
+          },
+          {
+            answer2: "HTTP",
+            answer_status: true,
+          },
+          {
+            answer3: "SMTP",
+            answer_status: false,
+          },
+          {
+            answer4: "SSH",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 12,
+        question_title: "Что такое IP-адрес",
+        answers: [
+          {
+            answer1: "УНИКАЛЬНЫЙ НОМЕР УСТРОЙСТВА В СЕТИ",
+            answer_status: true,
+          },
+          {
+            answer2: "ПАРОЛЬ ОТ WI-FI",
+            answer_status: false,
+          },
+          {
+            answer3: "АДРЕС ВЕБ-САЙТА",
+            answer_status: false,
+          },
+          {
+            answer4: "НОМЕР СОЕДИНЕНИЕ VPN",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 13,
+        question_title: "Что такое межсетевой экран (брандмауэр)?",
+        answers: [
+          {
+            answer1: "АНТИВИРУС",
+            answer_status: false,
+          },
+          {
+            answer2: "СПАМ-ФИЛЬТР",
+            answer_status: false,
+          },
+          {
+            answer3: "ЗАЩИТА ОТ ХАКЕРОВ",
+            answer_status: true,
+          },
+          {
+            answer4: "БЛОКИРОВКА ДОСТУПА К СЕТИ",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 14,
+        question_title:
+          "Как называется аппаратное обеспечение для соединения компьютеров в сеть?",
+        answers: [
+          {
+            answer1: "МАРШРУТИЗАТОР",
+            answer_status: true,
+          },
+          {
+            answer2: "МОНИТОР",
+            answer_status: false,
+          },
+          {
+            answer3: "ВИДЕОКАРТА",
+            answer_status: false,
+          },
+          {
+            answer4: "СЕТЕВАЯ КАРТА",
+            answer_status: false,
+          },
+        ],
+      },
+      {
+        id: 15,
+        question_title: "Что делает системный администратор?",
+        answers: [
+          {
+            answer1: "ПРОГРАММИРУЕТ",
+            answer_status: true,
+          },
+          {
+            answer2: "ОБСЛУЖИВАЕТ КОМПЬЮТЕРЫ И СЕТИ",
+            answer_status: false,
+          },
+          {
+            answer3: "РАБОТАЕТ В ТЕХПОДДЕРЖКЕ",
+            answer_status: false,
+          },
+          {
+            answer4: "ТЕСТИРУЕТ ПРИЛОДЕНИЕ",
+            answer_status: false,
+          },
+        ],
+      },
+    ],
+  },
+];
