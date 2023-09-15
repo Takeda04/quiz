bf49bb882c66041e659ab3cb410210fd06f8dea7
Alirahmon777 <alirahmonuraliev007@gmail.com>
2023-09-15 16:39:44 +0500

    lang

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import homeLogo from '../assets/logo.svg';
 import anonsy from '../assets/anonsy.png';
-import { Link } from 'react-router-dom';
 import { Button } from '../components';
 
 const Login = () => {
+  const lang = localStorage.getItem('lang');
   return (
     <div className='min-h-screen h-full bg-cover bg-[url(./assets/bg.png)] bg-[#000B23] text-white'>
       <div className='flex items-center justify-center w-full pt-[57px]'>
         <img src={homeLogo} alt='logo' />
       </div>
       <div className='flex flex-col gap-[16px] text-center uppercase mt-[77px]'>
-        <h2 className='text-[35px] font-extrabold'>Узнай кто ты в IT?</h2>
+        <h2 className='text-[35px] font-extrabold'>
+          {lang == 'uz' ? 'IT sohasida kim ekanligingizni bilib oling?' : 'Узнай кто ты в IT?'}
+        </h2>
         <p className='text-[12px] font-semibold'>
           пройди Интерактивный ТЕст и узнай какая специальность в IT, тебе подходит больше всего.
         </p>
